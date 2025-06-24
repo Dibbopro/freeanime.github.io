2afaedc4534633ca795b185d68ab17c8b8e6d3de
Dibbopro <94396845+Dibbopro@users.noreply.github.com>
2025-06-24 14:04:37 +0600

    Update Anime.jsx

--- public/src/pages/Anime.jsx
+++ public/src/pages/Anime.jsx
@@ -1,1 +1,20 @@
+const { animeId } = useParams();
+const [anime, setAnime] = useState(null);
 
+useEffect(() => {
+  fetch(`https://gogoanime.consumet.stream/anime-details/${animeId}`)
+    .then(res => res.json())
+    .then(setAnime);
+}, [animeId]);
+
+return (
+  <div>
+    <h1>{anime.title}</h1>
+    <iframe
+      src={`https://gogoplay.io/streaming.php?id=${anime.episodes[0].id}`}
+      allowFullScreen
+      width="100%"
+      height="500px"
+    />
+  </div>
+);
