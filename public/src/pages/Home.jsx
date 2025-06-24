cd369b4f7265ddf13ae21e38c81458b587936bb6
Dibbopro <94396845+Dibbopro@users.noreply.github.com>
2025-06-24 14:04:10 +0600

    Update Home.jsx

--- public/src/pages/Home.jsx
+++ public/src/pages/Home.jsx
@@ -1,1 +1,8 @@
+const [query, setQuery] = useState("");
+const [results, setResults] = useState([]);
 
+const searchAnime = async () => {
+  const res = await fetch(`https://gogoanime.consumet.stream/search?keyw=${query}`);
+  const data = await res.json();
+  setResults(data);
+};
